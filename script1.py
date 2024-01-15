9b01b246d3ab98940e9a2211fce84f6b127da933
Jacob Gallant <156605905+JacobGallant101@users.noreply.github.com>
2024-01-15 13:25:57 -0500

    Modified both scripts in the repo clone
    -Deleted a print() statement in script1.py
    - Added a new print() statement to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 #This script will print the lines chosen from the Zen of Python.
 print("Beautiful is better than ugly.")
-print("Explicit is better than implicit.")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 #Printing lines from Zen of Python.
 print("Special cases aren't special enough to break the rules.")
 print("Although practicality beats purity.")
+print("Errors should never pass silently.")
